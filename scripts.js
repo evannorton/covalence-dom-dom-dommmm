79cff0abd3e973f16d003c36b16d2ebd2f38427a
Evan Norton <evanan39@yahoo.com>
2018-01-11 10:24:52 -0600

    delete comments

--- scripts.js
+++ scripts.js
@@ -13,34 +13,3 @@ function button() {
         document.body.appendChild(square);
     }
 }
-
-/* document.addEventListener("DOMContentLoaded", paragraphText())
-document.addEventListener("DOMContentLoaded", button())
-
-function paragraphText() {
-    let paragraph = document.createElement("p");
-    let text = document.createTextNode("This can be whatever texxt you would like it to be");
-
-    paragraph.style.color = "red";
-    paragraph.style.textTransform = "uppercase";
-    paragraph.className = "some-paragraph";
-
-    paragraph.appendChild(text);
-    document.body.appendChild(paragraph); 
-}
-
-function button() {
-    let button = document.createElement("button");
-    let btnText = document.createTextNode("Click Me!");
-    button.appendChild(btnText);
-    document.body.appendChild(button);
-
-    button.addEventListener("click", clickButton);
-
-    function clickButton() {
-        let h1 = document.createElement("h1");
-        let h1Text = document.createTextNode("I've been clicked!");
-        h1.appendChild(h1Text);
-        document.body.appendChild(h1);
-    }
-} */
