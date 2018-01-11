81a5ede75896f571049e9c0b34fa3b23202d4dd2
Evan Norton <evanan39@yahoo.com>
2018-01-11 10:41:49 -0600

    fix syntax

--- scripts.js
+++ scripts.js
@@ -1,16 +1,14 @@
-document.addEventListener("DOMContentLoaded", button());
-
+document.addEventListener("DOMContentLoaded", button);
 function button() {
     let button = document.createElement("button");
     let btnText =  document.createTextNode("Add square!");
     button.appendChild(btnText);
     document.body.appendChild(button);
 
-    button.addEventListener("click", addSquare());
+    button.addEventListener("click", addSquare);
     function addSquare() {
         let square = document.createElement("div");
         square.className = "black-square";
         document.body.appendChild(square);
-        square.style.color = "black";
     }
 }
